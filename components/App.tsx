5719d77a2fcd88fe1348aa91e3e91629593b666f
jmanrocks152 <jmanrocks152@gmail.com>
2026-02-20 22:52:05 -0600

    added header

--- components/App.tsx
+++ components/App.tsx
@@ -5,8 +5,11 @@ import {crossword} from "@/public/example";
 
 export default function App() {
     return (
-        <div className="mt-5 ml-5 mr-5">
-            <Crossword data={crossword}/>
+        <div>
+            <h1 className="font-title text-heading text-2xl font-bold text-center mt-2">Mini Crossword</h1>
+            <div className="mt-2 ml-5 mr-5">
+                <Crossword data={crossword}/>
+            </div>
         </div>
     );
 }
